fc1411bde82e5aabcb1ce7ead552febdeb3c8d5e
msgarski <msgarski@gmail.com>
2021-05-26 14:33:30 +0200

    button podciągnięty i metody przeciążone

--- app/src/main/java/com/example/androidlab4/MainActivity.java
+++ app/src/main/java/com/example/androidlab4/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.androidlab4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -18,14 +22,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String[] values = new String[] { "Pies", "Kot", "Koń", "Gołąb", "Kruk", "Dzik", "Karp", "Osioł", "Chomik", "Mysz", "Jeż", "Kraluch" };        this.target = new ArrayList<String>();
+        String[] values = new String[] { "Pies", "Kot", "Koń", "Gołąb", "Kruk", "Dzik", "Karp", "Osioł", "Chomik", "Mysz", "Jeż", "Kraluch" };
+        this.target = new ArrayList<String>();
         this.target.addAll(Arrays.asList(values));
 //
         this.adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, this.target);
 //
         ListView listview = (ListView) findViewById( R.id.listview );
         listview.setAdapter(this.adapter);
-
-
     }
-}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+    @Override protected void onActivityResult( int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            String nowy = (String) extras.get("wpis");
+            target.add(nowy);
+            adapter.notifyDataSetChanged();
+        }
+    }
+
+
+        public void nowyWpis (MenuItem mi){
+            Intent intencja = new Intent(this, DodajWpis.class);
+            startActivityForResult(intencja, 1);
+        }
+    }
